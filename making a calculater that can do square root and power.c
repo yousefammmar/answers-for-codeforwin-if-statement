2b490cdd6c0db0372f72e768747b3cf7383ede6e
yousefammmar <123825108+yousefammmar@users.noreply.github.com>
2024-10-19 15:55:12 +0300

    Add files via upload

---
+++ making a calculater that can do square root and power.c
@@ -0,0 +1,44 @@
+//making a calculater that can do square root and power
+#include <stdio.h>
+#include <math.h>
+int main(){
+    char op;
+    int num1,num2,sum;
+    printf("if you want to find the square root enter the letter s\n");
+    printf("enter the operation you want to apply:");
+    scanf("%c",&op);
+    if(op=='+'||op=='-'||op=='*'||op=='/'||op=='^'){
+        printf("enter any two numbers:");
+        scanf("%d",&num1);
+        scanf("%d",&num2);
+        if(op=='+'){
+            sum=num1+num2;
+            printf("the answer of %d+%d=%d",num1,num2,sum);
+        }
+        else if(op=='-'){
+            sum=num1-num2;
+            printf("the answer of %d-%d=%d",num1,num2,sum);
+        }
+        else if(op=='/'&&num2!=0){
+            sum=num1/num2;
+            printf("the answer of %d/%d=%d",num1,num2,sum);
+        }
+        else if(op=='*'){
+            sum=num1/num2;
+            printf("the answer of %d*%d=%d",num1,num2,sum);
+        }
+        else if(op=='^'){
+            sum=pow(num1,num2);
+            printf("the answer of %d^%d=%d",num1,num2,sum);
+        }
+    }
+    else if(op=='s'){
+            float num,result;
+            
+            printf("enter any number:");
+            scanf("%f",&num);
+            result=sqrt(num);
+            printf("the square root for %0.2f=%0.2f",num,result);
+        }
+    return 0;
+}
