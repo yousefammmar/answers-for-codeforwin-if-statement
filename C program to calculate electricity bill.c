b253df4402aa74e80299c64d521b2ea5e555f382
yousefammmar <123825108+yousefammmar@users.noreply.github.com>
2024-12-12 12:45:56 +0300

    Update C program to calculate electricity bill.c

--- C program to calculate electricity bill.c
+++ C program to calculate electricity bill.c
@@ -7,10 +7,10 @@ int main (){
     if (unit<=50){
         amt=unit*0.5;
     }
-    else if (unit>50&&unit>=150){
+    else if (unit>50&&unit<=150){
         amt=25+(unit-50)*0.75;
     }
-    else if (unit>150&&unit>=250){
+    else if (unit>150&&unit<=250){
         amt=100+(unit-150)*1.2;
     }
     else if (unit>250){
